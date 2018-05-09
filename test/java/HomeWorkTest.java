fb08aae22974f011dd6d0cbe6b7fbb4ce31e916c
michalslowikowski <michalslowikowski00@gmail.com>
2018-05-09 18:31:26 +0200

    homework-test-cases

--- test/java/HomeWorkTest.java
+++ test/java/HomeWorkTest.java
@@ -4,62 +4,69 @@ import org.junit.Test;
 public class HomeWorkTest {
 
     @Test
-    public void DivisionNegativeNumber0(){
-        HomeWork divisionResult = new HomeWork();
-        divisionResult.setNumberToDivide((-32769 + 1));
-        divisionResult.setDivider(30000);
+    public void DivisionNegativeValue1(){
+        HomeWork divisionResult = new HomeWork((short) -32768, (short) 30000);
         assert divisionResult.DivideNumbers() == -1:
                 "Result should be: " + divisionResult.DivideNumbers();
-        System.out.println("Correct result of DivisionNegativeNumber0 is: " + divisionResult.DivideNumbers());
+        System.out.println("Correct result of DivisionNegativeValue1 is: " +
+                divisionResult.DivideNumbers());
     }
 
     @Test
-    public void DivisionNegativeNumber1(){
-        HomeWork divisionResult = new HomeWork();
-        divisionResult.setNumberToDivide((-32768 + 1));
-        divisionResult.setDivider(30000);
+    public void DivisionNegativeValue2(){
+        HomeWork divisionResult = new HomeWork((short) -32767, (short) 30000);
         assert divisionResult.DivideNumbers() == -1:
                 "Result should be: " + divisionResult.DivideNumbers();
-        System.out.println("Correct result of DivisionNegativeNumber1 is: " + divisionResult.DivideNumbers());
+        System.out.println("Correct result of DivisionNegativeValue2 is: " +
+                divisionResult.DivideNumbers());
     }
 
     @Test
-    public void DivisionNegativeNumber2(){
-        HomeWork divisionResult = new HomeWork();
-        divisionResult.setNumberToDivide((-32767 + 1));
-        divisionResult.setDivider(30000);
+    public void DivisionNegativeValue3(){
+        HomeWork divisionResult = new HomeWork((short) -1, (short) 30000);
+        assert divisionResult.DivideNumbers() == 0:
+                "Result should be: " + divisionResult.DivideNumbers();
+        System.out.println("Correct result of DivisionNegativeValue3 is: " +
+                divisionResult.DivideNumbers());
+    }
+
+    @Test
+    public void DivisionNegativeValue4(){
+        HomeWork divisionResult = new HomeWork((short) 0, (short) 30000);
+        assert divisionResult.DivideNumbers() == 0:
+                "Result should be: " + divisionResult.DivideNumbers();
+        System.out.println("Correct result of DivisionNegativeValue4 is: " +
+                divisionResult.DivideNumbers());
+    }
+
+    @Test
+    public void DivisionNegativeValue5(){
+        HomeWork divisionResult = new HomeWork((short) 1, (short) 30000);
+        assert divisionResult.DivideNumbers() == 0:
+                "Result should be: " + divisionResult.DivideNumbers();
+        System.out.println("Correct result of DivisionNegativeValue5 is: " +
+                divisionResult.DivideNumbers());
+    }
+
+    @Test
+    public void DivisionNegativeValue6(){
+        HomeWork divisionResult = new HomeWork((short) 32767, (short) 30000);
         assert divisionResult.DivideNumbers() == -1:
                 "Result should be: " + divisionResult.DivideNumbers();
-        System.out.println("Correct result of DivisionNegativeNumber2 is: " + divisionResult.DivideNumbers());
+        System.out.println("Correct result of DivisionNegativeValue6 is: " +
+                divisionResult.DivideNumbers());
     }
 
     @Test
-    public void DivisionPositiveNumber0(){
-        HomeWork divisionResult = new HomeWork();
-        divisionResult.setNumberToDivide((32768 + 1));
-        divisionResult.setDivider(30000);
+    public void DivisionNegativeValue7(){
+        HomeWork divisionResult = new HomeWork((short) 32766, (short) 30000);
         assert divisionResult.DivideNumbers() == 1:
                 "Result should be: " + divisionResult.DivideNumbers();
-        System.out.println("Correct result DivisionPositiveNumber0 is: " + divisionResult.DivideNumbers());
-    }
-
-    @Test
-    public void DivisionPositiveNumber1(){
-        HomeWork divisionResult = new HomeWork();
-        divisionResult.setNumberToDivide((32767 + 1));
-        divisionResult.setDivider(30000);
-        assert divisionResult.DivideNumbers() == 1:
-                "Result should be: " + divisionResult.DivideNumbers();
-        System.out.println("Correct result of DivisionPositiveNumber1 is: " + divisionResult.DivideNumbers());
-    }
-
-    @Test
-    public void DivisionPositiveNumber2(){
-        HomeWork divisionResult = new HomeWork();
-        divisionResult.setNumberToDivide((32766 + 1));
-        divisionResult.setDivider(30000);
-        assert divisionResult.DivideNumbers() == 1:
-                "Result should be: " + divisionResult.DivideNumbers();
-        System.out.println("Correct result DivisionPositiveNumber2 is: " + divisionResult.DivideNumbers());
+        System.out.println("Correct result of DivisionNegativeValue7 is: " +
+                divisionResult.DivideNumbers());
     }
 }
+
+
+
+
